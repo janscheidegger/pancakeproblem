4f3c4ece98085e629deb0480d4935946025848c5
Jan Scheidegger <scheideggerj@gmail.com>
2017-01-18 09:58:44 +0100

    print path

--- src/ch/bfh/pancake/PancakeProblem.java
+++ src/ch/bfh/pancake/PancakeProblem.java
@@ -32,32 +32,7 @@ public class PancakeProblem {
 
     public static void main(String... args) {
 
-        //parseInputArguments(args);
-
-        long start = System.nanoTime();
-        int[] pancakes = {2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15, 17};
-        int bound = gapHeuristic(pancakes);
-        int count;
-        do {
-            count = countSolutions(pancakes, bound);
-            bound++;
-            System.out.println("bound is now "+bound);
-        } while(count <= 0);
-        long end = System.nanoTime();
-        System.out.println((end - start)/1E9);
-        System.out.println(count);
-    }
-
-    private static void parseInputArguments(String[] args) {
-        Options options = new Options();
-
-        Option mode = new Option("m", "mode", true, "first or all");
-        mode.setRequired(true);
-        options.addOption(mode);
-
-        Option stack = new Option("s", "stackType", true, "random or pairwise");
-        stack.setRequired(true);
-        options.addOption(stack);
+        Options options = createOptions();
 
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -73,12 +48,68 @@ public class PancakeProblem {
             return;
         }
 
-        String inputFilePath = cmd.getOptionValue("input");
-        String outputFilePath = cmd.getOptionValue("output");
+        String mode = cmd.getOptionValue("mode");
+        String outputFilePath = cmd.getOptionValue("stackType");
 
-        System.out.println(inputFilePath);
-        System.out.println(outputFilePath);
 
+        long start = System.nanoTime();
+        int[] pancakes = {2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15, 17};
+        int bound = gapHeuristic(pancakes);
+        int count;
+        do {
+            if("count".equalsIgnoreCase(mode)) {
+                count = countSolutions(pancakes, bound);
+            } else {
+                pancakes = new int[]{2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 15};
+                count = findBestSolution(pancakes, bound);
+            }
+            bound++;
+            System.out.println("bound is now "+bound);
+        } while(count <= 0);
+        long end = System.nanoTime();
+        System.out.println((end - start)/1E9);
+        System.out.println(count);
+    }
+
+    private static int findBestSolution(int[] pancakes, int bound) {
+        Deque<PancakeNode> pancakeStack = new ArrayDeque<>();
+        pancakeStack.addFirst(new PancakeNode(pancakes, 0, 0, bound, new int[pancakes.length]));
+        int count = 0;
+
+        PancakeNode currentPancakes;
+        while (!pancakeStack.isEmpty()) {
+            currentPancakes = pancakeStack.removeFirst();
+            int f = currentPancakes.depth + currentPancakes.heuristic;
+            if (f > bound) continue;
+            if(currentPancakes.isSorted()) {
+                count++;
+                System.out.println(Arrays.toString(currentPancakes.cuts));
+                printSolution(pancakes, currentPancakes.cuts);
+                return count;
+            }
+            int nextDepth = currentPancakes.depth + 1;
+            for(int i = 2;i <= currentPancakes.pancakes.length; i++) {
+                if(i == currentPancakes.lastcut) continue;
+                int [] newPancakes = flipAt(currentPancakes.pancakes, i);
+                int h2 = gapHeuristic(newPancakes);
+                pancakeStack.addFirst(new PancakeNode(newPancakes, nextDepth, i, h2, currentPancakes.cuts));
+            }
+        }
+        return count;
+    }
+
+
+    private static Options createOptions() {
+        Options options = new Options();
+
+        Option mode = new Option("m", "mode", true, "first or all");
+        mode.setRequired(true);
+        options.addOption(mode);
+
+        Option stack = new Option("s", "stackType", true, "random or pairwise");
+        stack.setRequired(true);
+        options.addOption(stack);
+        return options;
     }
 
     private static int countSolutions(int [] pancakes, int bound) {
@@ -116,4 +147,20 @@ public class PancakeProblem {
         return temp;
     }
 
+
+    private static void printSolution(int[] pancakes, int[] flips) {
+        int i = 0;
+        do {
+            System.out.print("State "+i+": ");
+            for (int j = 0; j < flips[i];j++) {
+                System.out.print(pancakes[j]+", ");
+            }
+            System.out.print("| ");
+            for(int j = flips[i];j<pancakes.length; j++ ) {
+                System.out.print(pancakes[j]+", ");
+            }
+            System.out.println();
+            pancakes = flipAt(pancakes, flips[++i]);
+        } while (flips[i]!= 0);
+    }
 }
